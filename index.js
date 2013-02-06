3266563dae8df4c4fc2061c5fa19b376f608df18
Calvin French-Owen <calvin@segment.io>
2013-02-06 11:54:12 -0800

    Updating pathname to use a leading / for IE compatibility

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ exports.parse = function(url){
     port: a.port || location.port,
     hash: a.hash,
     hostname: a.hostname || location.hostname,
-    pathname: a.pathname,
+    pathname: a.pathname.charAt(0) != '/' ? '/' + a.pathname : a.pathname,
     protocol: !a.protocol || ':' == a.protocol ? location.protocol : a.protocol,
     search: a.search,
     query: a.search.slice(1)
